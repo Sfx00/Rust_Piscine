d9ba1f034ef0eafb24fee5465329f373882755ad
SFX <oussamaba348@gmail.com>
2025-08-01 11:01:52 +0100

    doner

---
+++ speed_transformation.rs
@@ -0,0 +1,9 @@
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    (km_h * 1000.0) / 3600.0
+}
+
+fn main() {
+    let km_h = 100.0;
+    let m_s = km_per_hour_to_meters_per_second(km_h);
+    println!("{} km/h is equivalent to {} m/s", km_h, m_s);
+}
