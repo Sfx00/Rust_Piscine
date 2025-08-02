81447baad75a4f58622c444914237c16fe57bf6a
SFX <oussamaba348@gmail.com>
2025-08-02 18:34:22 +0100

    done

---
+++ tuples.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Student(u32, String, String);
+
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}
+
+fn main() {
+    let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
+    println!("Student's first name: {}", first_name(&student));
+    println!("Student's last name: {}", last_name(&student));
+    println!("Student's id: {}", id(&student));
+}
