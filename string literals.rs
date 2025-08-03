909462007483a822d72e000ef31d86d52b929f88
SFX <oussamaba348@gmail.com>
2025-08-03 20:41:26 +0100

    done

---
+++ string literals.rs
@@ -0,0 +1,22 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+}
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+pub fn find(v: &str, pat: char) -> usize {
+    v.find(pat).unwrap_or(0)
+}
+fn main() {
+    println!("{}", is_empty(""));
+    println!("{}", is_ascii("rust"));
+    println!("{}", contains("rust", "ru"));
+    println!("{:?}", split_at("rust", 2));
+    println!("{}", find("rust", 'a'));
+}
